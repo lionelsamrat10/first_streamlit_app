17ae218d6f6b5a504b7942e920c495eee8d944a8
Samrat Mitra <samratmitra338@gmail.com>
2022-07-14 20:02:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
  
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
