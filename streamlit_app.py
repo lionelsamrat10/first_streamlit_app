506848e0ca1469c4072e3bc0681642a29a73c884
Samrat Mitra <samratmitra338@gmail.com>
2022-07-14 14:19:07 +0530

    APi Call header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 st.dataframe(fruits_to_show)
 
+# New section to display fruityvice API Response
+st.header("Fruityvice Fruit Advice!")
 # Calling API from Streamlit
 import requests as req
 fruityvice_response = req.get("https://fruityvice.com/api/fruit/watermelon")
